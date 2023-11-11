a97531cb9c50eb30f0f7b8846b06a2a97168e7ae
IngAliElkadri <elkadri2002@gmail.com>
2023-11-11 00:48:26 -0400

    Ruta reportarpedido mas o menos buena

--- basedatos/consultas.js
+++ basedatos/consultas.js
@@ -37,7 +37,7 @@ async function registrarUsu(correo, usuario, clave, nadmin, sucursal) { //REGIST
     }
   } 
 async function reProducto(nombre,descripcion,monto){ // CONSULTA PARA INSERTAR PRODUCTOS
-    const consulta = 'INSERT INTO productos(id, nombre, descripcion, precio) VALUES (,?,?,?)';
+    const consulta = 'INSERT INTO productos(nombre, descripcion, precio) VALUES (?,?,?)';
     const conexion =await conectarBaseDeDatos()
     try{
         const result = await conexion.query(consulta,[nombre,descripcion,monto]);
@@ -62,7 +62,7 @@ async function comExiPro(nombre, descripcion) { //COMPROBAR EXISTENCIA PRODUCTO
       conexion.end();
       console.log('CONEXION CERRADA');
       if (result.length > 0) {
-        return true;
+        return result;
       } else {
         return false;
       }
@@ -83,6 +83,19 @@ async function ExisUsuario(nombre) {
       } else {
         return [true, { Mensaje: 'Usuario disponible' }];
       }
+    } catch (error) {
+      console.log('Error al verificar si el usuario está disponible:', error);
+      throw error; // COMPROBAR NOMBRE USUARIO SI ESTA EXISTENTE
+    }
+  }
+  async function Repago(id_reportante,referencia,monto,banco_id,estado_id,responsable_estado,fecha_aceptado) {
+    const consulta = 'INSERT INTO pagos(id_reportante, referencia, monto, banco_id, estado_id, responsable_estado, fecha_aceptado) VALUES (?,?,?,?,?,?,?)';
+    const conexion =await conectarBaseDeDatos()
+    try {
+      const result = await conexion.query(consulta, [id_reportante,referencia,monto,banco_id,estado_id,responsable_estado,fecha_aceptado]);
+      conexion.end();
+      console.log('CONEXION CERRADA');
+      console.log('Producto registrado con exito');
     } catch (error) {
       console.log('Error al verificar si el usuario está disponible:', error);
       throw error; // COMPROBAR NOMBRE USUARIO SI ESTA EXISTENTE
@@ -123,8 +136,89 @@ async function obtenerUltimoToken(id){
     throw error; // Obtener ultimo token de usuario
   }
 }
+async function VpagosUsu(id_reportante){
+  const consulta = 'SELECT p.id,p.referencia,p.monto, e.estado AS nombre_estado, u.usuario AS nombre_responsable FROM pagos p JOIN estados e ON p.estado_id = e.id JOIN usuarios u ON p.responsable_estado = u.id WHERE p.id_reportante = ? AND DATE(p.fecha_emision) = CURDATE();';
+  const conexion =await conectarBaseDeDatos()
+  try {
+    const result = await conexion.query(consulta, [id_reportante]);
+    conexion.end();
+    console.log('CONEXION CERRADA');
+    if (result.length > 0) {
+      return result
+    } else {
+      return [{ Mensaje: 'No existen reportes de pagos' }];
+    }
+  } catch (error) {
+    console.log('Error al obtener registro de pagos:', error);
+    throw error; 
+  }
+}
+async function Recliente(nombre,apellido,cedula,telefono,direccion) {
+  const consulta = 'INSERT INTO clientes(nombre, apellido, cedula, telefono, direccion) VALUES (?,?,?,?,?)';
+  const conexion =await conectarBaseDeDatos()
+  try {
+    const result = await conexion.query(consulta, [nombre,apellido,cedula,telefono,direccion]);
+    conexion.end();
+    console.log('CONEXION CERRADA');
+    console.log('cliente registrado con exito');
+  } catch (error) {
+    console.log('Error al verificar si el usuario está disponible:', error);
+    throw error; // COMPROBAR NOMBRE USUARIO SI ESTA EXISTENTE
+  }
+}
+async function Redelivery(id_pedido,nombre_delivery) {
+  const consulta = 'INSERT INTO deliverys(id_pedido, nombre_delivery) VALUES (?,?)';
+  const conexion =await conectarBaseDeDatos()
+  try {
+    const result = await conexion.query(consulta, [id_pedido,nombre_delivery]);
+    conexion.end();
+    console.log('CONEXION CERRADA');
+    console.log('Delivery registrado con exito');
+  } catch (error) {
+    console.log('Error al registrar delivery:', error);
+    throw error; 
+  }
+}
+async function comExicliente(cedula) { //COMPROBAR EXISTENCIA cliente
+  const consulta = 'SELECT * FROM clientes WHERE cedula=?';
+  const conexion =await conectarBaseDeDatos()
+  try {
+    const result = await conexion.query(consulta, [cedula]);
+    conexion.end();
+    console.log('CONEXION CERRADA');
+    if (result.length > 0) {
+      return result;
+    } else {
+      return false;
+    }
+  } catch (error) {
+    console.log('Error en base de datos:', error);
+    throw error; 
+  }
+}
+async function Repedido(id_pedido,id_cliente,id_reportante,id_producto,cantidad,precio_u,estado_pedido) {
+  const consulta = 'INSERT INTO pedidos(id_pedido, id_cliente, id_reportante, id_producto, cantidad, precio_u, estado_pedido) VALUES (?,?,?,?,?,?,?)';
+  const conexion =await conectarBaseDeDatos()
+  try {
+    const result = await conexion.query(consulta, [id_pedido,id_cliente,id_reportante,id_producto,cantidad,precio_u,estado_pedido]);
+    conexion.end();
+    console.log('CONEXION CERRADA');
+    console.log('pedido registrado con exito');
+  } catch (error) {
+    console.log('Error al registrar pedido:', error);
+    throw error; 
+  }
+}
 module.exports ={
     comprobarlogeo,
     registrarToken,
-    obtenerUltimoToken
+    obtenerUltimoToken,
+    Repago,
+    VpagosUsu,
+    Recliente,
+    comExiPro,
+    reProducto,
+    Redelivery,
+    comExicliente,
+    Repedido
 }
--- rutas.js
+++ rutas.js
@@ -4,7 +4,7 @@ const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
 const consultasbd = require('./basedatos/consultas');
 const secretkey = 'AF84565599ALI';
-
+const bodyParser = require('body-parser');
 async function verificaToken(token, secretkey, informacionPe = {}) {
   try {
     const decodedToken = jwt.verify(token, secretkey);
@@ -29,7 +29,7 @@ async function manejarToken(usuarioId, secretkey) {
       const registrartoken = await consultasbd.registrarToken(usuarioId, token, '1h');
       return tokenString;
     }
-    return {token,tokenString};
+    return tokenString;
   } else {
     // Si no hay un token existente, genera uno nuevo
     const informacion = { usuarioId };
@@ -49,9 +49,9 @@ ruter.post('/inicioSesion', async (req, res) => {
     const resultado = operacion[1][0][0];
     if (resultado) {
       const token = await manejarToken(resultado.id, secretkey);
-      res.send({ token,resultado });
+      res.status(200).send({ token,resultado });
     } else {
-      res.status(404).json({ message: 'Credenciales incorrectas' });
+      res.status(200).send({ message: 'Credenciales incorrectas' });
     }
   } catch (error) {
     console.error('Error al iniciar sesión:', error);
@@ -60,12 +60,84 @@ ruter.post('/inicioSesion', async (req, res) => {
 });
 
 // RUTA INICIO USUARIO (sin el middleware autenticarToken)
-ruter.post('/inicio/usuario', async (req, res) => {
+ruter.post('/usuario/reportarpagos', async (req, res) => {
   try {
-    // Acción para la ruta de inicio de usuario
+    const { id_reportante, referencia,monto, banco_id, estado_id, responsable_estado, fecha_aceptado } = req.body;
+    const operacion = await consultasbd.Repago(id_reportante, referencia,monto, banco_id, estado_id, responsable_estado, fecha_aceptado);
+    res.status(200).json({Message:"Producto registrado con exito"});
   } catch (error) {
-    console.error('Error accediendo a inicio usuario', error);
-    res.status(500).json({ message: 'Error en /inicio/usuario' });
+    console.error('Error registrando un pago', error);
+    res.status(500).json({ message: 'Error en /reportarpagos' });
+  }
+});
+ruter.get('/usuario/verpagos/:id_reportante', async (req, res) => {
+  try {
+    const id_reportante= req.params.id_reportante;
+    const operacion = await consultasbd.VpagosUsu(id_reportante);
+    if (operacion && operacion.length > 1 && operacion[0].length > 0) {
+      res.status(200).json(operacion[0]);
+    }
+    else{
+      res.status(200).json({Message: "No hay reportes"});
+    }
+  } catch (error) {
+    console.error('Error viendo reportes de pagos', error);
+    res.status(500).json({ message: 'Error en /usuario/verpagos/' });
+  }
+});
+ruter.post('/usuario/reportarpedido/', async (req, res) => {
+  try {
+    const { id_reportante,cantidad_prod,//datos reportante
+     nombre,apellido,cedula,telefono,direccion,//datos cliente
+     nombre_producto,descripcion,precio } =req.body //datos productos
+    //falta pedido debe ser manejada la informacion en la consulta sql
+    //const {id_pedido,nombre_delivery} = req.body //datos delivery
+    var comprobarexitecliente = await consultasbd.comExicliente(cedula);
+    var id_cliente;
+    var verificarprod;
+    var id_producto;
+    var reportarpedido;
+    var registrarProducto;
+    var registrarcliente;
+    console.log(comprobarexitecliente[0][0]);
+    if (comprobarexitecliente[0][0]){
+      verificarprod = await consultasbd.comExiPro(nombre_producto,descripcion)
+     // console.log(verificarprod);
+      id_cliente=await consultasbd.comExicliente(cedula);
+      if (verificarprod[0][0]){ //si existe el producto
+        id_producto =verificarprod[0][0].id;
+        reportarpedido=consultasbd.Repedido(cedula,id_cliente,id_reportante,id_producto,cantidad_prod,precio,1);
+        res.status(200).json('Pedido registrado exitosamente');
+      }
+      else{
+        registrarProducto= await consultasbd.reProducto(nombre_producto,descripcion,precio);
+        reportarpedido=await consultasbd.Repedido(cedula,id_cliente,id_reportante,id_producto,cantidad_prod,precio,1);
+        res.status(200).json('Pedido registrado exitosamente');
+      }
+    }
+    else{
+      console.log('esto es else');
+      registrarcliente = await consultasbd.Recliente(nombre,apellido,cedula,telefono,direccion);
+      comprobarexitecliente = await consultasbd.comExicliente(cedula);
+      id_cliente=comprobarexitecliente[0][0].id;
+      console.log(id_cliente);
+      verificarprod = await consultasbd.comExiPro(nombre_producto,descripcion)
+      console.log(verificarprod)
+      if (verificarprod && verificarprod.length > 1 && verificarprod[0].length > 0){ //si existe el producto
+        reportarpedido=await consultasbd.Repedido(cedula,id_cliente,id_reportante,id_producto,cantidad_prod,precio,1);
+        console.log('esto es reportar pedido', reportarpedido);
+       }
+       else{
+         await consultasbd.reProducto(nombre_producto,descripcion,precio);
+         verificarprod = await consultasbd.comExiPro(nombre_producto,descripcion)
+         id_producto =verificarprod[0][0].id;
+         await consultasbd.Repedido(cedula,id_cliente,id_reportante,id_producto,cantidad_prod,precio,1);
+         console.log('Producto registrado exitosamente')
+       }
+    }
+  } catch (error) {
+    console.error('Error viendo reportes de pagos', error);
+    res.status(500).json({ message: 'Error en /usuario/reportarpedido/' });
   }
 });
 
